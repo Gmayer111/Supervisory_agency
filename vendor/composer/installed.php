74bfed54d1f6b001ba68d39dfe229b08b103addc
Gael Mayer <gaelmayer@yahoo.fr>
2021-09-14 10:05:00 +0200

    composer adding

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '42bdf39b5a91817ea484b03caf52fd651995ec74',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '42bdf39b5a91817ea484b03caf52fd651995ec74',
+    ),
+    'heroku/heroku-buildpack-php' => 
+    array (
+      'pretty_version' => 'v197',
+      'version' => '197.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0b77e568edae1c65d7cfb2f56db80291ca8688cf',
+    ),
+  ),
+);
