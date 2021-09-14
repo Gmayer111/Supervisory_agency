6b7077740d42b227a781935d70a7902b14fc81d0
Gael Mayer <gaelmayer@yahoo.fr>
2021-09-14 22:45:51 +0200

    config jawsdb url

--- Managers/MissionManager.php
+++ Managers/MissionManager.php
@@ -13,12 +13,13 @@ class MissionManager
     public function connect(): PDO
     {
         if (getenv('JAWSDB_URL') !== false) {
-            $dbparts = parse_url(getenv('JAWSDB_URL'));
-
+            $url = getenv('JAWSDB_URL');
+            $dbparts = parse_url($url);
             $hostname = $dbparts['host'];
             $username = $dbparts['user'];
             $password = $dbparts['pass'];
             $database = ltrim($dbparts['path'],'/');
+
 
         }else {
             $username = 'root';
