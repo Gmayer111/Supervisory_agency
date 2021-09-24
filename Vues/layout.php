923f7f1f8c119061f60983e62fd17c9bebc4b15b
Gael Mayer <gaelmayer@yahoo.fr>
2021-09-24 19:20:21 +0200

    font added

--- Vues/layout.php
+++ Vues/layout.php
@@ -9,6 +9,7 @@ $missions = $manager->getAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://kit.fontawesome.com/b5efc1d547.js" crossorigin="anonymous"></script>
+    <link href='http://fonts.googleapis.com/css?family=Fututa' rel='stylesheet' type='text/css'>
     <title><?php echo $title; ?></title>
 </head>
 <body>
@@ -18,32 +19,32 @@ $missions = $manager->getAll();
         <div>
             <ul class="liste">
                 <?php if (isset($_SESSION['CodeName'])): ?>
-                    <li><a href="index.php">Accueil</a></li>
-                    <li><a href="?action=Profil">Profil</a></li>
-                    <li><a href="?action=MissionForm">Créer mission</a></li>
-                    <li><a href="?action=List">Missions</a></li>
+                    <li><a href="index.php">ACCUEIL</a></li>
+                    <li><a href="?action=Profil">PROFIL</a></li>
+                    <li><a href="?action=MissionForm">CRÉER MISSION</a></li>
+                    <li><a href="?action=List">MISSIONS</a></li>
                     <div class="dropdown">
-                        <button class="dropBtn">Détail mission</button>
+                        <button class="dropBtn">DÉTAIL MISSION</button>
                         <div class="dropContent">
                             <?php foreach ($missions as $mission): ?>
                                 <a href="?action=Detail&codeName=<?php echo $mission->getCodeName() ?>"><?php echo $mission->getCodeName() ?></a>
                             <?php endforeach; ?>
                         </div>
                     </div>
-                    <li><a href="?action=Logout" class="btn-login">DÉCONNEXION</a></li>
+                    <li><a href="?action=Logout" class="btn-login btnLogD">DÉCONNEXION</a></li>
                 <?php endif; ?>
                 <?php if (!isset($_SESSION['CodeName'])): ?>
-                    <li><a href="index.php">Accueil</a></li>
-                    <li><a href="?action=List">Missions</a></li>
+                    <li><a href="index.php">ACCUEIL</a></li>
+                    <li><a href="?action=List">MISSIONS</a></li>
                     <div class="dropdown">
-                        <button class="dropBtn btnH">Détail mission</button>
+                        <button class="dropBtn btnH">DÉTAIL MISSION</button>
                         <div class="dropContent">
                             <?php foreach ($missions as $mission): ?>
                                 <a href="?action=Detail&codeName=<?php echo $mission->getCodeName() ?>"><?php echo $mission->getCodeName() ?></a>
                             <?php endforeach; ?>
                         </div>
                     </div>
-                    <li><a href="?action=Logout" class="btn-login">CONNEXION</a></li>
+                    <li><a href="?action=Logout" class="btn-login btnLogC">CONNEXION</a></li>
                 <?php endif; ?>
             </ul>
         </div>
